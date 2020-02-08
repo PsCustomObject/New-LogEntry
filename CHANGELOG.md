1f3a7f17021ff678e9c855b47efcb19a85073b81
PsCustomObject <PsCustomObject@outlook.com>
2020-02-08 10:49:24 +0100

    Released New-LogEntry version 2.0.0
    A complete list of changes in code is available in https://github.com/PsCustomObject/New-LogEntry/blob/master/CHANGELOG.md
    Reorganized ChangeLog file
    Fixed typo in ChangeLog file
    
    Signed-off-by: PsCustomObject <PsCustomObject@outlook.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,36 @@
 # New-LogEntry - Change History
 
-- 1.1.2 - Time stamp will now include year in the format MM-dd-yy
-- 1.1.1 - Function now supports *ShouldProcess* directive
-  - Introduced support for passing log messages via PipeLine
-  - Implemented additional aliases
-  - Minor cose style cleanup
-- 1.1.0 - Removed unused code
-  - Updated Header section
-  - Splatted commands for better readability
-  - Implemented  **NoTimeStamp** parameter to suppress log timestamp
-  - Fixed an issue causing Write-Error not to be correctly called
-  - Various code optimizations
-- 1.0.0 - Initial Release
+## Version 2.0.0
+
+- Function now uses [Mutex objects](https://docs.microsoft.com/en-us/windows/win32/sync/mutex-objects) to avoid situations where function was called before lock on log file was released causing exceptions to be thrown and log lines to be missed
+- Code rewritten from scratch optimized for exeuction time
+- Implemented the *[Info]* tag by default prepended to all log messages when not using the *-IsError* or *-IsWarning* parameters
+- Optimized function to write log messages to a buffer rather than a file stream
+- Implemented *-BufferOnlyInfo*, *-BufferOnlyWarning* and *BufferOnlyError* to better handle redirection of messages to buffer rather than a file
+- Log messages will now be printed to console by default unless the *-NoConsole* parameter is used to allow easier troubleshooting of runtime scripts
+- Updated timestamp format to use **MM/dd/yyyy hh:mm:ss tt** format to clearly indicate AM/PM time of the log
+- Fixed an issue causnig function to throw an exception when log filename contained special characters like *[*
+
+## Version 1.1.2
+
+- Time stamp will now include year in the format MM-dd-yy
+
+## Version 1.1.1
+
+- Function now supports *ShouldProcess* directive
+- Introduced support for passing log messages via PipeLine
+- Implemented additional aliases
+- Minor case style cleanup
+
+## Version 1.1.0
+
+- Removed unused code
+- Updated Header section
+- Splatted commands for better readability
+- Implemented  **NoTimeStamp** parameter to suppress log timestamp
+- Fixed an issue causing Write-Error not to be correctly called
+- Various code optimizations
+
+## Version 1.0.0
+
+- Initial Release
--- New-LogEntry.ps1
+++ New-LogEntry.ps1
@@ -1,206 +1,245 @@
 ﻿function New-LogEntry
 {
 <#
-	.SYNOPSIS
-		Function to create a log file for PowerShell scripts
-	
-	.DESCRIPTION
-		Function supports both writing to a text file (default), sending messages only to console via ConsoleOnly parameter or both via WriteToConsole parameter.
-		
-		The BufferOnly parameter will not write message neither to console or logfile but save to a temporary buffer which can then be piped to file or printed to screen.
-	
-	.PARAMETER logMessage
-		A string containing the message PowerShell should log for example about current action being performed.
-	
-	.PARAMETER WriteToConsole
-		Writes the log message both to the log file and the interactive
-		console, similar to built-in Write-Host.
-	
-	.PARAMETER logFilePath
-		Specifies the path and log file name that will be created.
-		
-		Parameter only accepts full path IE C:\MyLog.log
-	
-	.PARAMETER isErrorMessage
-		Prepend the log message with the [Error] tag in file and
-		uses the Write-Error built-in cmdlet to throw a non terminating
-		error in PowerShell Console
-	
-	.PARAMETER IsWarningMessage
-		Prepend the log message with the [Warning] tag in file and
-		uses the Write-Warning built-in cmdlet to throw a warning in
-		PowerShell Console
-	
-	.PARAMETER ConsoleOnly
-		Print the log message to console without writing it file
-	
-	.PARAMETER BufferOnly
-		Saves log message to a variable without printing to console
-		or writing to log file
-	
-	.PARAMETER SaveToBuffer
-		Saves log message to a variable for later use
-	
-	.PARAMETER NoTimeStamp
-		Suppresses timestamp in log message
-	
-	.EXAMPLE
-		Example 1: Write a log message to log file
-		PS C:\> New-LogEntry -LogMessage "Test Entry"
-		
-		This will simply output the message "Test Entry" in the logfile
-		
-		Example 2: Write a log message to console only
-		PS C:\> New-LogEntry -LogMessage "Test Entry" -ConsoleOnly
-		
-		This will print Test Entry on console
-		
-		Example 3: Write an error log message
-		New-LogEntry -LogMessage "Test Log Error" -isErrorMessage
-		
-		This will prepend the [Error] tag in front of
-		log message like:
-		
-		[06-21 03:20:57] : [Error] - Test Log Error
-	
-	.NOTES
-		Function supports both PowerShell and PowerShell core
+    .SYNOPSIS
+        Function serves as logging framework for PowerShell scripts.
+    
+    .DESCRIPTION
+        Function allows writing log messages to a log file that can be located locally or an UNC Path additionally using buffers is supported
+        to allow loggin in situations where PsProvider does not allow access to the local system for example when working with SCCM cmdlets.
+        
+        By default all log messages are prepended with the [INFO] tag, see -IsError or -IsWarning parameters for more details on additional tags,
+        unless the -NoTag parameter is specified.
+    
+    .PARAMETER LogMessage
+        A string representing the message to be written to the lot stream.
+    
+    .PARAMETER LogFilePath
+        A string representing the path and file name to be used for writing log messages.
+        
+        If parameter is not specified $PSCommandPath will be used.
+    
+    .PARAMETER IsErrorMessage
+        When parameter is specified log message will be prepended with the [Error] tag additionally Write-Error will be used to print
+        error on console.
+    
+    .PARAMETER IsWarningMessage
+        When parameter is specified log message will be prepended with the [Warning] tag additionally Write-Warning will be used to print
+        error on console.
+    
+    .PARAMETER BufferOnlyInfo
+        When parameter is specified log message will be saved to a temporary log-buffer with script scope for later retrieval.
+    
+    .PARAMETER NoTimeStamp
+        When parameter is specified timestamp will omitted in the log message.
+    
+    .PARAMETER NoConsole
+        When parameter is specified console output will be suppressed.
+    
+    .PARAMETER BufferOnlyWarning
+        When parameter is specified log message will be saved to a temporary log-buffer with script scope for later retrieval and
+        message will be repended with the [Warning] tag
+    
+    .PARAMETER BufferOnlyError
+        When parameter is specified log message will be saved to a temporary log-buffer with script scope for later retrieval and
+        message will be repended with the [Error] tag
+    
+    .PARAMETER BufferOnly
+        A description of the BufferOnly parameter.
+    
+    .PARAMETER NoTag
+        When parameter is specified tag representing message severity will be not be part of the log message.
+    
+    .EXAMPLE
+        PS C:\> Add-LogEntry
+    
+    .OUTPUTS
+        string, string, void, object, string
+    
+    .NOTES
+        Additional information about the function.
 #>
-	
-	[CmdletBinding(ConfirmImpact = 'High',
-				   PositionalBinding = $true,
-				   SupportsShouldProcess = $true)]
-	param
-	(
-		[Parameter(Mandatory = $true,
-				   ValueFromPipeline = $true)]
-		[AllowNull()]
-		[Alias('Log', 'Message')]
-		[string]
-		$LogMessage,
-		[Alias('Print', 'Echo', 'Console')]
-		[switch]
-		$WriteToConsole = $false,
-		[AllowNull()]
-		[Alias('Path', 'LogFile', 'File', 'LogPath')]
-		[string]
-		$LogFilePath,
-		[Alias('Error', 'IsError', 'WriteError')]
-		[switch]
-		$IsErrorMessage = $false,
-		[Alias('Warning', 'IsWarning', 'WriteWarning')]
-		[switch]
-		$IsWarningMessage = $false,
-		[Alias('EchoOnly')]
-		[switch]
-		$ConsoleOnly = $false,
-		[switch]
-		$BufferOnly = $false,
-		[switch]
-		$SaveToBuffer = $false,
-		[Alias('Nodate', 'NoStamp')]
-		[switch]
-		$NoTimeStamp = $false
-	)
-	
-	# Use script path if no filepath is specified
-	if (([string]::IsNullOrEmpty($logFilePath) -eq $true) -and
-		(!($ConsoleOnly)))
-	{
-		$logFilePath = $PSCommandPath + '-LogFile-' + $(Get-Date -Format 'yy-MM-dd') + '.log'
-	}
-	
-	# Don't do anything on empty Log Message
-	if ([string]::IsNullOrEmpty($logMessage) -eq $true)
-	{
-		return
-	}
-	
-	# Format log message
-	if (($isErrorMessage) -and
-		(!($ConsoleOnly)))
-	{
-		if ($NoTimeStamp)
-		{
-			$tmpMessage = "[Error] - $logMessage"
-		}
-		else
-		{
-			$tmpMessage = "[$(Get-Date -Format 'MM-dd-yy hh:mm:ss')] : [Error] - $logMessage"
-		}
-	}
-	elseif (($IsWarningMessage -eq $true) -and
-		(!($ConsoleOnly)))
-	{
-		if ($NoTimeStamp)
-		{
-			$tmpMessage = "[Warning] - $logMessage"
-		}
-		else
-		{
-			$tmpMessage = "[$(Get-Date -Format 'MM-dd-yy hh:mm:ss')] : [Warning] - $logMessage"
-		}
-	}
-	else
-	{
-		if (!($ConsoleOnly))
-		{
-			if ($NoTimeStamp)
-			{
-				$tmpMessage = $logMessage
-			}
-			else
-			{
-				$tmpMessage = "[$(Get-Date -Format 'MM-dd-yy hh:mm:ss')] : $logMessage"
-			}
-		}
-	}
-	
-	# Write log messages to console
-	if (($ConsoleOnly) -or
-		($WriteToConsole))
-	{
-		if ($IsErrorMessage)
-		{
-			Write-Error $logMessage
-		}
-		elseif ($IsWarningMessage)
-		{
-			Write-Warning $logMessage
-		}
-		else
-		{
-			Write-Output -InputObject $logMessage
-		}
-		
-		# Write to console and exit
-		if ($ConsoleOnly -eq $true)
-		{
-			return
-		}
-	}
-	
-	# Write log messages to file
-	if (([string]::IsNullOrEmpty($logFilePath) -eq $false) -and
-		($BufferOnly -ne $true))
-	{
-		$paramOutFile = @{
-			InputObject = $tmpMessage
-			FilePath    = $LogFilePath
-			Append	    = $true
-			Encoding    = 'utf8'
-		}
-		
-		Out-File @paramOutFile
-	}
-	
-	# Save message to buffer
-	if (($BufferOnly -eq $true) -or
-		($SaveToBuffer -eq $true))
-	{
-		$script:messageBuffer += $tmpMessage + '`r`n'
-		
-		# Remove blank lines
-		$script:messageBuffer = $script:messageBuffer -creplace '(?m)^\s*\r?\n', ''
-	}
+    
+    [CmdletBinding(DefaultParameterSetName = 'Info')]
+    [OutputType([string], ParameterSetName = 'Info')]
+    [OutputType([string], ParameterSetName = 'Error')]
+    [OutputType([string], ParameterSetName = 'Warning')]
+    [OutputType([string], ParameterSetName = 'NoConsole')]
+    [OutputType([string], ParameterSetName = 'BufferOnly')]
+    param
+    (
+        [Parameter(ParameterSetName = 'Error')]
+        [Parameter(ParameterSetName = 'Info')]
+        [Parameter(ParameterSetName = 'NoConsole')]
+        [Parameter(ParameterSetName = 'Warning',
+                   Mandatory = $true)]
+        [Parameter(ParameterSetName = 'BufferOnly')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Log', 'Message')]
+        [string]
+        $LogMessage,
+        [Parameter(ParameterSetName = 'Error',
+                   Mandatory = $false)]
+        [Parameter(ParameterSetName = 'Info')]
+        [Parameter(ParameterSetName = 'Warning')]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $LogFilePath,
+        [Parameter(ParameterSetName = 'Error')]
+        [Alias('IsError', 'WriteError')]
+        [switch]
+        $IsErrorMessage,
+        [Parameter(ParameterSetName = 'Warning')]
+        [Alias('Warning', 'IsWarning', 'WriteWarning')]
+        [switch]
+        $IsWarningMessage,
+        [Parameter(ParameterSetName = 'BufferOnly')]
+        [switch]
+        $BufferOnlyInfo,
+        [Parameter(ParameterSetName = 'Error')]
+        [Parameter(ParameterSetName = 'Info')]
+        [Parameter(ParameterSetName = 'NoConsole')]
+        [Parameter(ParameterSetName = 'Warning')]
+        [switch]
+        $NoTimeStamp,
+        [Parameter(ParameterSetName = 'Error')]
+        [Parameter(ParameterSetName = 'Info')]
+        [Parameter(ParameterSetName = 'Warning')]
+        [Parameter(ParameterSetName = 'NoConsole')]
+        [switch]
+        $NoConsole,
+        [Parameter(ParameterSetName = 'BufferOnly')]
+        [switch]
+        $BufferOnlyWarning,
+        [Parameter(ParameterSetName = 'BufferOnly')]
+        [switch]
+        $BufferOnlyError,
+        [Parameter(ParameterSetName = 'BufferOnly')]
+        [switch]
+        $BufferOnly,
+        [Parameter(ParameterSetName = 'BufferOnly')]
+        [Parameter(ParameterSetName = 'Error')]
+        [Parameter(ParameterSetName = 'Info')]
+        [Parameter(ParameterSetName = 'NoConsole')]
+        [Parameter(ParameterSetName = 'Warning')]
+        [switch]
+        $NoTag
+    )
+    
+    begin
+    {
+        # Instantiate new mutex to implement lock
+        [System.Threading.Mutex]$logMutex = New-Object System.Threading.Mutex($false, 'LogSemaphore')
+        
+        # Check if file locked
+        [void]$logMutex.WaitOne()
+        
+        # Get current date timestamp
+        [string]$currentDate = [System.DateTime]::Now.ToString('[MM/dd/yyyy hh:mm:ss tt]')
+        
+        # Use script path if no filepath is specified
+        if ([string]::IsNullOrEmpty($LogFilePath) -eq $true)
+        {
+            # Generate log file path and name
+            $LogFilePath = '{0}{1}{2}{3}' -f $PSCommandPath, '-LogFile-', $currentDate, '.log'
+        }
+    }
+    
+    process
+    {
+        # Initialize commandsplat 
+        $paramOutFile = @{
+            LiteralPath = $LogFilePath
+            Append      = $true
+            Encoding    = 'utf8'
+        }
+        
+        switch ($PsCmdlet.ParameterSetName)
+        {
+            'Info'
+            {
+                # Format log message
+                [string]$tmpLogMessage = '{0} - [INFO]: {1}' -f $currentDate, $LogMessage
+                
+                # Append to log
+                $paramOutFile.Add('InputObject', $tmpLogMessage)
+                
+                # Suppress console output
+                if (!($NoConsole))
+                {
+                    Write-Output -InputObject $tmpLogMessage
+                }
+                
+                Out-File @paramOutFile
+                
+                break
+            }
+            'Warning'
+            {
+                # Format log message
+                [string]$tmpLogMessage = '{0} - [WARNING]:  {1}' -f $currentDate, $LogMessage
+                
+                # Append to log
+                $paramOutFile.Add('InputObject', $tmpLogMessage)
+                
+                # Suppress console output
+                if (!($NoConsole))
+                {
+                    Write-Warning -Message $tmpLogMessage
+                }
+                
+                Out-File @paramOutFile
+                
+                break
+            }
+            'Error'
+            {
+                # Format log message
+                [string]$tmpLogMessage = '{0} - [ERROR]: {1}' -f $currentDate, $LogMessage
+                
+                # Append to log
+                $paramOutFile.Add('InputObject', $tmpLogMessage)
+                
+                # Suppress console output
+                if (!($NoConsole))
+                {
+                    Write-Error -Message $tmpLogMessage -Category 'WriteError'
+                }
+                
+                Out-File @paramOutFile
+                
+                break
+            }
+            
+            'BufferOnly' {
+                
+                switch ($PSBoundParameters.Keys)
+                {
+                    'BufferOnlyWarning'
+                    {
+                        # Format log message
+                        [string]$tmpLogMessage = '{0} - [WARNING]: {1}' -f $currentDate, $LogMessage
+                    }
+                    'BufferOnlyError'
+                    {
+                        # Format log message
+                        [string]$tmpLogMessage = '{0} - [ERROR]: {1}' -f $currentDate, $LogMessage
+                    }
+                    default
+                    {
+                        # Format log message
+                        [string]$tmpLogMessage = '{0} - [INFO]: {1}' -f $currentDate, $LogMessage
+                    }
+                }
+                
+                # Format message for buffer
+                [string]$script:messageBuffer += $tmpLogMessage + [Environment]::NewLine
+                
+                break
+            }
+        }
+    }
 }
+#DONE: Add category to write-error as https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/write-error?view=powershell-7
+#TODO: Implement -NoTag parameter
+#TODO: Implement CategoryReason in error message
